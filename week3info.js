54b011fc150c382b6f4bc63394487a98e26b6be2
pugnaguido <144164356+pugnaguido@users.noreply.github.com>
2025-07-30 15:56:17 -0700

    Add files via upload

---
+++ week3info.js
@@ -0,0 +1,13 @@
+      //single line comment
+                /*
+                    multi line comment
+                */
+                    var firstName = "skyler"// sometimes you give the variable a starting value
+                    let lastName; //sometimes you dont know the variable value till later. you need to still create it.  
+                    // this is valid java script but dont do this
+                    //when you create a variable like this it is not clear where the variable comes into exisctence first. 
+                   // pizza = "yummy";
+                   let pizza = "yummy"
+                   //javasript is dunamically or loosley typed so javasscript guesses what the datatype is for youre variables
+                   //this can cause issues with adding nuymber if javascripr thinks youre numbers are strings. 
+                   document.getElementById("mainsubs").textContent = "Im an advancing computer science major expected to graduate december of 2026";
